85e83e0a819a07791bf179b5197a0bcab1e2008b
monika11081987 <bansal11mona@gmail.com>
2024-04-04 17:31:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import {useSelector,useDispatch} from 'react-redux';
+import { getCatsFetch } from './catState';
 import './App.css';
 
 function App() {
+  const cats=useSelector(state=>state.cats.cats);
+
+const dispatch=useDispatch();
+
+useEffect(()=>{
+  dispatch(getCatsFetch());
+},[dispatch]);
+console.log(cats);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CAT SPECIES GALLERY</h1>
+      <p>Images of different species of cats.</p>
+      <hr/>
+      <div className='Gallery'>
+        {cats.map(cat=>
+          <div key={cat.id} className='row'>
+            <div className='column column-left'>
+              {/* <img alt={cat.name}
+              src={cat.image.url}
+              width="200"
+              height="200"
+              /> */}
+              {cat.name}
+            </div>
+            <div className='column column-right'>
+              <h2>Temperament:{cat.temperament}</h2>
+              <p>{cat.description}</p>
+            </div>
+
+          </div>
+        )}
+      </div>
+      
+      <button>view more cats</button>
     </div>
   );
 }
